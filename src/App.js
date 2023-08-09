18385b5f129dfbdc1dc78dfcc69039c3d4abc020
Looi Wei En <looi.weien02@student.usm.my>
2023-08-09 16:36:48 +0800

    add tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import Task from './Task';
-import TaskForm from './TaskForm';
+import Task from './component/Task';
+import TaskForm from './component/TaskForm';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 
 
 function ToDoApp() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+
   const [tasks, setTasks] = useState([]);
-
-
 
   useEffect(() => {
     if (tasks.length === 0)
