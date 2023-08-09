413749ed0fe21720a28a00fd22fa9915c1b116b9
Looi Wei En <looi.weien02@student.usm.my>
2023-08-09 16:48:49 +0800

    remove dark mode codes

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import { useEffect, useState } from 'react';
 
 
 function ToDoApp() {
-  const [theme, setTheme] = useState(null);
-
-  useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark');
-    }
-    else {
-      setTheme('light');
-    }
-  }, [])
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const handleThemeSwitch = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
-
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
